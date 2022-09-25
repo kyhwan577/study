fbb5e304aa7649fda80d36a2e83ba84f5ca6c292
cylee85 <cylee@nextron.co.kr>
2022-09-26 08:57:16 +0900

    라벨 추가

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from PySide2.QtWidgets import QApplication, QWidget
+from PySide2.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
 
 
 app = QApplication([])
 widget = QWidget()
 widget.setWindowTitle("Company")
+label = QLabel("Add")
+layout = QVBoxLayout(widget)
+layout.addWidget(label)
 widget.show()
 
 app.exec_()
