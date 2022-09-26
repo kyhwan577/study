98ffe874129caede7c9865a3a60e9158ed393956
cylee85 <cylee@nextron.co.kr>
2022-09-26 09:02:02 +0900

    컴퍼니2로 변경

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from PySide2.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton
 
 app = QApplication([])
 widget = QWidget()
-widget.setWindowTitle("Company")
+widget.setWindowTitle("Company2")
 btn = QPushButton('push')
 layout = QVBoxLayout(widget)
 layout.addWidget(btn)
